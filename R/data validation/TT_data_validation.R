e0c43a4c0eaa5007f3e8d5f2914d845e02777a71
Junyi-Wu <81131396+Junyi-Wu@users.noreply.github.com>
2026-01-14 23:27:46 +0800

    updated splist 20260114

--- R/data validation/TT_data_validation.R
+++ R/data validation/TT_data_validation.R
@@ -7,7 +7,7 @@ sapply(usepackage, library, character.only = TRUE)
 
 
 # (1) 假設你有一個 modified_date 變數；如果沒有，就直接指定檔名。
-modified_date <- "20260106"  # 舉例
+modified_date <- "20260114"  # 舉例
 
 # (2) 讀取檔案 & 篩選欄位
 df_TTsplist <- fread(sprintf("../../data/input/TT/TTsplist_%s.csv", modified_date), sep = ",", fill=TRUE, encoding = "UTF-8", colClasses="character", header=TRUE)
@@ -371,10 +371,16 @@ fwrite(df_errors, "../../data/output/TT_errortypes_result.csv")
 
 df_TT_attribute <- df_TTsplist %>%
   select(
-    taxonUUID, taxonRank, kingdom, simplifiedScientificName, 
+    taxonUUID, taiCOLNameCode, taxonRank, kingdom, simplifiedScientificName, 
     endemism, nativeness,               
     protectedStatusTW, categoryRedlistTW, categoryIUCN, sensitiveCategory
     )
+
+df_TT_withouttcnamecode <- df_TT_attribute %>%
+  filter(
+    taiCOLNameCode == ""
+  )
+df_TT_withouttcnamecode$reason <- "taiCOLNameCode空白"
 
 df_TT_undertaxon <- df_TT_attribute %>%
   filter(
@@ -437,7 +443,7 @@ df_TT_animalia <- df_TT_attribute %>%
 df_TT_animalia$reason <- "原生性等於外來栽培or外來歸化的動物"
 
 
-df_TT_attribute_error <- rbind(df_TT_undertaxon, df_TT_redlist, df_TT_protected, df_TT_IUCN, df_TT_invasive, df_TT_animalia)
+df_TT_attribute_error <- rbind(df_TT_withouttcnamecode, df_TT_undertaxon, df_TT_redlist, df_TT_protected, df_TT_IUCN, df_TT_invasive, df_TT_animalia)
 # ✅ 在這裡加判斷：完全包住 for 迴圈
 
 
