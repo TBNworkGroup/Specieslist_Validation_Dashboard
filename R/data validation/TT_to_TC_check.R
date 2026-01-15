56773fffa9653b03c32be1d0b1dd8ea77a18a574
Junyi-Wu <81131396+Junyi-Wu@users.noreply.github.com>
2026-01-15 16:13:05 +0800

    add TT to TC process 20260114

---
+++ R/data validation/TT_to_TC_check.R
@@ -0,0 +1,76 @@
+rm(list=ls())
+
+usepackage <- c("jsonlite", "tidyverse", "data.table")
+install.packages(usepackage[!(usepackage %in% installed.packages()[,1])])
+sapply(usepackage, library, character.only = TRUE)
+
+# (1) 假設你有一個 modified_date 變數；如果沒有，就直接指定檔名。
+modified_date <- "20260114"  # 舉例
+
+# (2) 讀取檔案 & 篩選欄位
+df_TTsplist <- fread(sprintf("../../data/input/TT/TTsplist_%s.csv", modified_date), sep = ",", fill=TRUE, encoding = "UTF-8", colClasses="character", header=TRUE)
+
+df_TCsplist <- fread(sprintf("../../data/input/TC/TCsplist_%s.csv", modified_date), sep = ",", fill=TRUE, encoding = "UTF-8", colClasses="character", header=TRUE) %>% 
+  filter(taxon_status%in%"accepted")
+
+
+
+df_TT_select <- df_TTsplist %>%
+  select(taxonUUID, taxonRank, kingdom, taiCOLNameCode, simplifiedScientificName) %>% 
+  setnames(., c("taxonUUID", "taxonRank", "kingdom", "taiCOLNameCode", "simplifiedScientificName"), c("TT_taxonUUID", "TT_taxonRank", "TT_kingdom", "TT_taiCOLNameCode", "TT_simplifiedScientificName"))
+
+df_TC_select <- df_TCsplist %>%
+  select(taxon_id, rank, kingdom, simple_name)%>%
+  setnames(., c("taxon_id", "simple_name", "rank", "kingdom"), c("TC_taxon_id", "TC_simple_name", "TC_rank", "TC_kingdom")) %>% 
+  .[, TC_rank := tolower(TC_rank)]
+
+
+
+# ------------------------------------------------------------------
+# Part A: TT與TC，taxon_id、simplifiedScientificName、rank、kindgdom完全一致的分類群
+# ------------------------------------------------------------------
+TT_TC_all_same <- df_TT_select %>%
+  inner_join(
+    df_TC_select,
+    by = c(
+      "TT_simplifiedScientificName" = "TC_simple_name",
+      "TT_taxonRank"               = "TC_rank",
+      "TT_kingdom"                 = "TC_kingdom",
+      "TT_taiCOLNameCode"          = "TC_taxon_id"
+    )
+  )
+
+fwrite(TT_TC_all_same, "../../data/output/TT_to_TC/TT_nochange.csv")
+
+# ------------------------------------------------------------------
+# Part B: TT與TC，simplifiedScientificName、rank、kindgdom完全一致的分類群
+# ------------------------------------------------------------------
+
+TT_checkrank_taxon_id <- df_TT_select[!(TT_taxonUUID %in% as.vector(TT_TC_all_same$TT_taxonUUID))]%>%
+  inner_join(
+    df_TC_select,
+    by = c(
+      "TT_simplifiedScientificName" = "TC_simple_name",
+      "TT_taxonRank"               = "TC_rank",
+      "TT_kingdom"                 = "TC_kingdom"
+    )
+  )
+
+fwrite(TT_checkrank_taxon_id, "../../data/output/TT_to_TC/TT_checkrank_taxon_id.csv")
+# ------------------------------------------------------------------
+# Part C: TT與TC，simplifiedScientificName、rank、kindgdom完全一致的分類群
+# ------------------------------------------------------------------
+
+TT_checkrank_scientificName <- df_TT_select[!(TT_taxonUUID %in% as.vector(TT_TC_all_same$TT_taxonUUID))]%>%
+  .[!(.$TT_taxonUUID %in%  as.vector(TT_checkrank_taxon_id$TT_taxonUUID))] %>% 
+  inner_join(
+    df_TC_select,
+    by = c(
+      "TT_taxonRank"               = "TC_rank",
+      "TT_kingdom"                 = "TC_kingdom",
+      "TT_taiCOLNameCode"          = "TC_taxon_id"
+    )
+  )
+
+fwrite(TT_checkrank_scientificName, "../../data/output/TT_to_TC/TT_checkrank_scientificName.csv")
+
