f0dfb0b4e9048416a7bc003c5e5f0dcadc332eac
Junyi-Wu <81131396+Junyi-Wu@users.noreply.github.com>
2026-04-16 10:58:17 +0800

    updated splist 20240416

--- R/data validation/TT_to_TC_check.R
+++ R/data validation/TT_to_TC_check.R
@@ -5,7 +5,7 @@ install.packages(usepackage[!(usepackage %in% installed.packages()[,1])])
 sapply(usepackage, library, character.only = TRUE)
 
 # (1) 假設你有一個 modified_date 變數；如果沒有，就直接指定檔名。
-modified_date <- "20260408"  # 舉例
+modified_date <- "20260416"  # 舉例
 
 # (2) 讀取檔案 & 篩選欄位
 df_TTsplist <- fread(sprintf("../../data/input/TT/TTsplist_%s.csv", modified_date), sep = ",", fill=TRUE, encoding = "UTF-8", colClasses="character", header=TRUE)
